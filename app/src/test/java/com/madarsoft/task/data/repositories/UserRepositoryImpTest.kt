e91cb7690f527370832d87991b190bf26339233d
Ali hrera <alihrhera1@gmail.com>
2025-06-28 18:32:42 +0300

    Test: Refactor UserRepositoryImpTest to use Mockito

--- app/src/test/java/com/madarsoft/task/data/repositories/UserRepositoryImpTest.kt
+++ app/src/test/java/com/madarsoft/task/data/repositories/UserRepositoryImpTest.kt
@@ -3,21 +3,21 @@ package com.madarsoft.task.data.repositories
 import com.madarsoft.task.core.BaseDataResponse
 import com.madarsoft.task.data.local.users.UserEntity
 import com.madarsoft.task.data.local.users.UsersDao
-import io.mockk.coEvery
-import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
 import kotlin.test.assertEquals
 
 @ExperimentalCoroutinesApi
 class UserRepositoryImpTest {
 
     private lateinit var userRepository: UserRepositoryImp
-    private val mockUsersDao = mockk<UsersDao>()
+    private val mockUsersDao = mock(UsersDao::class.java)
 
     @Before
     fun setUp() {
@@ -28,10 +28,12 @@ class UserRepositoryImpTest {
     fun `test addUser success`() = runTest {
         val userEntity = UserEntity(name = "John", age = 25, gender = "Male", jobTitle = "Developer")
         val mockUserId = 1L
-        coEvery { mockUsersDao.addRow(any()) } returns mockUserId
+
+        `when`(mockUsersDao.addRow(userEntity)).thenReturn(mockUserId)
         val result = userRepository.addUser(userEntity)
         val user = result.last()
         assertTrue(user is BaseDataResponse.Success)
         assertEquals((user as BaseDataResponse.Success).data.id, mockUserId.toInt())
     }
 }
+
